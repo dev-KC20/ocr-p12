03e2803dfc4094dd4114c3604da92144bf337ff3
dev@KC <dev@kitconseil.fr>
2022-06-27 19:43:25 +0200

    chore: initiate project and app

---
+++ eecrm/users/urls.py
@@ -0,0 +1,12 @@
+# users/urls.py
+
+from django.urls import path
+from users.views import CreateUserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("signup/", CreateUserViewSet.as_view({"get": "create"}), name="signup"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
